13cce462bcaecd8be07587f29daf475281a07a54
Nikita Dobrov <nikitadobrov@gmail.com>
2021-05-24 18:20:54 +0300

    Change endpoint to ringl.co

--- ios/app/src/AppDelegate.m
+++ ios/app/src/AppDelegate.m
@@ -36,7 +36,7 @@
     jitsiMeet.defaultConferenceOptions = [JitsiMeetConferenceOptions fromBuilder:^(JitsiMeetConferenceOptionsBuilder *builder) {
         [builder setFeatureFlag:@"resolution" withValue:@(360)];
         [builder setFeatureFlag:@"ios.screensharing.enabled" withBoolean:YES];
-        builder.serverURL = [NSURL URLWithString:@"https://meet.jit.si"];
+        builder.serverURL = [NSURL URLWithString:@"https://ringl.co/"];
         builder.welcomePageEnabled = YES;
 
         // Apple rejected our app because they claim requiring a
